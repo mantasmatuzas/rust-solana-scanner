a885010abe491dd7fe02a37595bb8596046fabcf
Mantas Matūzas <mantas.matuzas@ennovatek.lt>
2025-03-23 22:39:56 +0200

    fix: solana by block api logging

--- src/web/routes_solana.rs
+++ src/web/routes_solana.rs
@@ -6,16 +6,16 @@ use axum::{Json, Router};
 
 pub fn routes(solana: Solana) -> Router {
     Router::new()
-        .route("/solana/{slot}", get(get_block))
+        .route("/solana/{slot}", get(get_solana_block_by_slot))
         .with_state(solana)
 }
 
-async fn get_block(
+async fn get_solana_block_by_slot(
     State(solana): State<Solana>,
     ctx: Ctx,
     Path(slot): Path<u64>,
 ) -> crate::Result<Json<SolanaBlock>> {
-    println!("->> {:<12} - list_tickets", "HANDLER");
+    println!("->> {:<12} - get_solana_block_by_slot", "HANDLER");
 
     let mut block = solana.get_block(slot).await?;
 
