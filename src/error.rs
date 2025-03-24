9cce6f4cc62b3bb40221c0a083f7ed4eafd1da65
Mantas Matūzas <mantas.matuzas@ennovatek.lt>
2025-03-24 22:28:47 +0200

    feat: add service to client error mapping and request/response logging mapper

--- src/error.rs
+++ src/error.rs
@@ -2,10 +2,12 @@
 
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
+use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
@@ -21,10 +23,51 @@ pub enum Error {
     SolanaBlockNotFound { slot: u64 },
 }
 
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAILED,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
+
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder Axum response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAILED),
+
+            // -- Auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenInvalidFormat
+            | Self::AuthFailCtxNotInRequestExtension => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+
+            // -- Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // -- Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@
 
 pub use self::error::{Error, Result};
 
+use crate::ctx::Ctx;
+use crate::log::log_request;
 use crate::ticket_model::ModelController;
 use axum::extract::{Path, Query};
-use axum::http::Response;
-use axum::response::{Html, IntoResponse};
+use axum::http::{Method, Uri};
+use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::{Router, middleware};
+use axum::{Json, Router, middleware};
 use serde::Deserialize;
+use serde_json::json;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod crypto;
 mod ctx;
 mod error;
+mod log;
 mod ticket_model;
 mod web;
 
@@ -55,9 +60,40 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn main_response_mapper<T>(res: Response<T>) -> Response<T> {
-    println!("->> {:<12} - main_response_mapper", "HANDLER");
+async fn main_response_mapper(
+    ctx: Result<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    // -- Get the eventual response
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // -- If client error, build the new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string()
+                }
+            });
+
+            println!("    ->> client_error_body: {client_error_body}");
+
+            // Build the new response from client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    let client_error = client_status_error.unzip().1;
+    let _ = log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+    println!("    ->> server log line - {uuid} - Error: {service_error:?}");
 
     println!();
-    res
+    error_response.unwrap_or(res)
 }
